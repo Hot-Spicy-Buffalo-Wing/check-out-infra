71fe59a54b201ec219cfaa6fa575195208160d2d
2paperstar <lee@paperst.ar>
2024-06-01 01:49:01 +0900

    fix: remove lightsail

--- key-pairs.tf
+++ key-pairs.tf
@@ -3,11 +3,6 @@ variable "main_public_key" {
   sensitive = true
 }
 
-resource "aws_lightsail_key_pair" "main_public_key" {
-  name       = "check-out-main-public-key"
-  public_key = var.main_public_key
-}
-
 resource "aws_key_pair" "main_public_key" {
   key_name   = "check-out-main-public-key"
   public_key = var.main_public_key
