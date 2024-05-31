1635cdc48033f0abb6a31a583e9e7d2fc02cacda
2paperstar <lee@paperst.ar>
2024-05-31 23:29:28 +0900

    fix: ec2 security group

--- ec2.tf
+++ ec2.tf
@@ -85,8 +85,8 @@ resource "aws_security_group" "k3s-security-group" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
