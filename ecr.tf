6b3d224393bfe580ac9ceb51803a198744da519a
2paperstar <lee@paperst.ar>
2024-06-01 16:23:20 +0900

    chore: add ecr, iam

---
+++ ecr.tf
@@ -0,0 +1,22 @@
+locals {
+  ecr_names = toset([
+    "check-out-backend",
+    "check-out-ai",
+  ])
+}
+
+resource "aws_ecr_repository" "ecr_repositories" {
+  for_each             = local.ecr_names
+  name                 = each.key
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+output "ecr_repositories" {
+  value = {
+    for k, v in aws_ecr_repository.ecr_repositories : k => v.repository_url
+  }
+}
---
+++ iam.tf
@@ -0,0 +1,59 @@
+resource "aws_iam_policy" "github_ecr_push" {
+  for_each = aws_ecr_repository.ecr_repositories
+  name     = "github_ecr_push_${each.key}"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        Resource = "${each.value.arn}:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group" "github_push" {
+  name = "github_push"
+}
+
+resource "aws_iam_user" "github_push" {
+  for_each = local.ecr_names
+  name     = "github_push_${each.key}"
+}
+
+resource "aws_iam_user_policy_attachment" "github_push" {
+  for_each   = local.ecr_names
+  user       = aws_iam_user.github_push[each.key].name
+  policy_arn = aws_iam_policy.github_ecr_push[each.key].arn
+}
+
+resource "aws_iam_group_membership" "github_push" {
+  name  = "github_push"
+  users = [for k in aws_iam_user.github_push : k.name]
+  group = aws_iam_group.github_push.name
+}
+
+resource "aws_iam_access_key" "github_push" {
+  for_each = aws_iam_user.github_push
+  user     = each.value.name
+}
+
+output "github_actions_access_secret" {
+  value = {
+    for k in aws_iam_access_key.github_push : k.user => {
+      access_key = k.id,
+      secret_key = k.secret
+    }
+  }
+  sensitive = true
+}
