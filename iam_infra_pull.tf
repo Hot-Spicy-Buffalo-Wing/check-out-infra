a65f4b5d2b0ceccd9c98f3f826f0873c19c533f8
2paperstar <lee@paperst.ar>
2024-06-02 01:25:20 +0900

    fix: pull iam

--- iam_infra_pull.tf
+++ iam_infra_pull.tf
@@ -6,14 +6,13 @@ resource "aws_iam_policy" "infra_ecr_pull" {
       {
         Effect = "Allow",
         Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
           "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
           "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload"
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
         ],
         Resource = "*",
       },
@@ -29,8 +28,8 @@ resource "aws_iam_user" "github_pull" {
   name = "github_pull"
 }
 
-resource "aws_iam_user_policy_attachment" "github_pull" {
-  user       = aws_iam_user.github_pull.name
+resource "aws_iam_group_policy_attachment" "github_pull" {
+  group      = aws_iam_group.github_pull.name
   policy_arn = aws_iam_policy.infra_ecr_pull.arn
 }
 
